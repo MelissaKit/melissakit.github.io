7e346dbaa835b73738f96e2e1184fb23aad9de6b
Mare <voz.15.1998@gmail.com>
2019-04-02 21:58:46 +0300

    commit 11 (gallery)

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ $('#menuToggle input').on('click', function (e) {
 });
 
 $('.modal, #menuToggle').show();
+
+$('.gallery').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.thumbs'
+});
+
+$('.thumbs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.gallery',
+    dots: true,
+    arrows: false,
+    centerMode: false,
+    focusOnSelect: true
+});
