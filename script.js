c518cff0c31387ba87f7d8d635bf834c2e711d67
Mare <voz.15.1998@gmail.com>
2019-04-11 22:03:52 +0300

    commit 12 (gallery + footer)

--- script.js
+++ script.js
@@ -6,20 +6,40 @@ $('#menuToggle input').on('click', function (e) {
 
 $('.modal, #menuToggle').show();
 
-$('.gallery').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    asNavFor: '.thumbs'
+var sliders = {
+    1: {slider : '.gallery.brown', nav: '.brown + .thumbs'},
+    2: {slider : '.gallery.red', nav: '.red + .thumbs'},
+    3: {slider : '.gallery.blue', nav: '.blue + .thumbs'}
+};
+
+
+
+$.each(sliders, function() {
+
+    $(this.slider).slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: this.nav,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $(this.nav).slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: this.slider,
+        dots: true,
+        arrows: false,
+        centerMode: false,
+        focusOnSelect: true
+    });
 });
 
-$('.thumbs').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.gallery',
-    dots: true,
-    arrows: false,
-    centerMode: false,
-    focusOnSelect: true
-});
